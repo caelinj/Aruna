d6021e213d2e6060f885d148652afa79cd1b4588
caelinj <caelinj@yandex.com>
2018-08-28 09:41:11 +1000

    Config changes, activity changing, ratelimit warning

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,3 +1,14 @@
+const { activityChangeRate, activities } = require('../config');
+
+const changeActivity = async (activities) => {
+    const [name, type] = activities[Math.floor(Math.random() * activities.length)];
+    client.user.setActivity(`${name}`, { type: type });
+}
+
 module.exports = (client) => {
     console.log(`Arura v${require('../../package.json').version} has started successfully! Logged in as ${client.user.tag} [${client.user.id}], on ${client.guilds.size} guild${client.guilds.size > 1 ? 's' : ''}.`);
+
+    changeActivity(activities);
+    if (activityChangeRate <= 29) console.warn(`Warning: Activity changing is disabled due to the minimum change rate (30 seconds) being exceeded`)
+    else client.setInterval(() => changeActivity(activities), activityChangeRate * 1000);
 }
